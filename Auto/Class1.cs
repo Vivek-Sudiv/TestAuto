fafb686359b91d712bd5ab9d9361044ea3286eb7
Vivek Sudiv <vsudiv@rmplc.net>
2019-05-08 17:33:25 +0530

    Add project files.

---
+++ Auto/Class1.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Auto
+{
+    public class Class1
+    {
+        [Fact]
+        public void Test1()
+        {
+            IWebDriver webdriver = new ChromeDriver();
+            webdriver.Navigate().GoToUrl("https://www.amazon.in/");
+            webdriver.FindElement(By.Id("twotabsearchtextbox")).SendKeys("i phone xs max 512 gb");
+            webdriver.FindElement(By.ClassName("nav-search-submit")).Click();
+            Thread.Sleep(2000);
+            webdriver.FindElement(By.XPath(".//span[contains(text(), 'Apple iPhone Xs (512GB) - Silver')]")).Click();
+            Thread.Sleep(2000);
+            webdriver.Quit();
+        }
+    }
+}
